b89b4f3570ca5c5167eaf81d0f705e4d043e3613
THAINA OLIVEIRA ARAUJO <thaina.oaraujo1@senacsp.edu.br>
2022-11-09 11:49:11 -0300

    first commit

---
+++ src/controllers/usuarioController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { Usuario } from '../models/Usuario';
+
+export const index = async (req:Request, res:Response) =>{
+    let users = await Usuario.findAll()
+    res.render("pages/usuarios",{
+        users
+    })
+} 
